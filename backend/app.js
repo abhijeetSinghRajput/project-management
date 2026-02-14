9fb7be44908c590eec738d19d45d0617e4f7cbe1
Abhijeet Singh Rajput <abhijeet62008@gmail.com>
2026-02-14 18:35:04 +0530

    update the backend/index.js

--- backend/app.js
+++ backend/app.js
@@ -59,7 +59,7 @@ export const createApp = (options = {}) => {
   // });
 
   // Serve frontend in production
-  if (ENV.NODE_ENV === "production") {
+  if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
     app.get("*", (req, res) => {
