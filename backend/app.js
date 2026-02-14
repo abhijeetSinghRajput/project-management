03a7415d51ca417129cfd41de0f0f1a87e745119
Abhijeet Singh Rajput <abhijeet62008@gmail.com>
2026-02-14 19:00:50 +0530

    update index.js

--- backend/app.js
+++ backend/app.js
@@ -63,13 +63,11 @@ export const createApp = () => {
   // ==============================
   // SERVE FRONTEND IN PRODUCTION
   // ==============================
-  if (process.env.NODE_ENV === "production") {
-    const frontendPath = path.join(__dirname, "../../frontend/dist");
-
-    app.use(express.static(frontendPath));
+  if (ENV.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
     app.get("*", (req, res) => {
-      res.sendFile(path.join(frontendPath, "index.html"));
+      res.sendFile(path.join(__dirname, "../../frontend", "dist", "index.html"));
     });
   } else {
     // 404 handler (only for API when not production frontend route)
